03f0bf619c218141517da6371a0407957f64d5d3
9Diagen <9diagen@gmail.com>
2023-11-17 17:20:44 +0500

    First Commit

--- src/main/java/diag/playmine/acolor/Color/Color.java
+++ src/main/java/diag/playmine/acolor/Color/Color.java
@@ -6,19 +6,14 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import java.text.MessageFormat;
 
 public class Color {
-    public Color(YamlConfiguration config) {
-        this.config = config;
-    }
-
-    public String c(String text) {
+    public static YamlConfiguration config;
+    public static String c(String text) {
         return ChatColor.translateAlternateColorCodes('&', text);
     }
-    public YamlConfiguration config;
-    public String message(String a, String[] args){
+    public static String message(String a, String[] args){
         return (MessageFormat.format(c(config.getString(a)), args));
     }
-
-    public String jsonMessage(String a, String[] args) {
+    public static String jsonMessage(String a, String[] args) {
         return (String.format(c(config.getString(a)), args));
     }
 }
